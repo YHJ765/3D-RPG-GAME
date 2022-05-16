5440fef6d250eb9a5ae70f1c47bc37bdf982ea9b
YHJ765 <765815153@qq.com>
2022-05-16 15:34:35 +0800

    Editor Script 自己写一个插件出来-2

--- Assets/Scripts/Editor/DialogueEditor.cs
+++ Assets/Scripts/Editor/DialogueEditor.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEditorInternal;
+using System;
 
 [CustomEditor(typeof(DialogueData_SO))]
 public class DialogueCustomEditor : Editor
@@ -19,6 +21,8 @@ public class DialogueCustomEditor : Editor
 public class DialogueEditor : EditorWindow 
 {
     DialogueData_SO currentData;
+
+    ReorderableList piecesList = null;
 
     [MenuItem("New Tools/Dialogue Editor")]
     
@@ -45,5 +49,87 @@ public class DialogueEditor : EditorWindow
             return true;
         }
         return false;
+    }
+
+    void OnGUI()
+    {
+        if(currentData != null)
+        {
+            EditorGUILayout.TextField(currentData.name, EditorStyles.boldLabel);
+            GUILayout.Space(10);
+
+            if(piecesList == null)
+                SetupReorderableList();
+
+            piecesList.DoLayoutList();
+        }
+        else
+        {
+            GUILayout.Label("NO DATA SELECTED!", EditorStyles.boldLabel);
+        }
+    }
+
+    private void SetupReorderableList()
+    {
+        piecesList = new ReorderableList(currentData.dialoguePieces, typeof(DialoguePiece), true, true, true, true);
+
+        piecesList.drawHeaderCallback += OnDrawPieceHeader;
+        piecesList.drawElementCallback += OnDrawPieceListElement;
+        piecesList.elementHeightCallback += OnHeightChanged;
+    }
+
+    private float OnHeightChanged(int index)
+    {
+       return GetPieceHeight(currentData.dialoguePieces[index]);
+    }
+
+    float GetPieceHeight(DialoguePiece piece)
+    {
+        var height = EditorGUIUtility.singleLineHeight;
+
+        height += EditorGUIUtility.singleLineHeight * 9;
+
+        return height;
+    }
+
+    private void OnDrawPieceListElement(Rect rect, int index, bool isActive, bool isFocused)
+    {
+        if(index < currentData.dialoguePieces.Count)
+        {
+            var currentPiece = currentData.dialoguePieces[index];
+
+            var tempRect = rect;
+
+            tempRect.height = EditorGUIUtility.singleLineHeight;
+
+            tempRect.width = 30;
+            EditorGUI.LabelField(tempRect, "ID");
+
+            tempRect.x += tempRect.width;
+            tempRect.width = 100;
+            currentPiece.ID = EditorGUI.TextField(tempRect, currentPiece.ID);
+
+            tempRect.x += tempRect.width + 10;
+            EditorGUI.LabelField(tempRect, "Quest");
+
+            tempRect.x += 45;
+            currentPiece.quest = (QuestData_SO)EditorGUI.ObjectField(tempRect, currentPiece.quest, typeof(QuestData_SO), false);
+
+            tempRect.y += EditorGUIUtility.singleLineHeight + 5;
+            tempRect.x = rect.x;
+            tempRect.height = 60;
+            tempRect.width = tempRect.height;
+            currentPiece.image = (Sprite)EditorGUI.ObjectField(tempRect, currentPiece.image, typeof(Sprite), false);
+
+            //文本框作业
+            tempRect.x += tempRect.width + 5;
+            tempRect.width = rect.width - tempRect.x;
+            currentPiece.text = (string)EditorGUI.TextField(tempRect, currentPiece.text);
+        }
+    }
+
+    private void OnDrawPieceHeader(Rect rect)
+    {
+        GUI.Label(rect, "Dialogue Pieces");
     }
 }
